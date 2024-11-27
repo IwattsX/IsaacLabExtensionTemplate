42fdb3743c2c20b6b6e7db4cb6a7dfe41b6bc7e5
= <Isaac.s.watts@gmail.com>
2024-11-26 21:50:00 -0500

    Deleted files that should of been ignored

--- ws_moveit/build/rrr_launcher_demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iwatts/Documents/IsaacLabExtensionTemplate/ws_moveit/src/rrr_launcher_demo
-# Build directory: /home/iwatts/Documents/IsaacLabExtensionTemplate/ws_moveit/build/rrr_launcher_demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
